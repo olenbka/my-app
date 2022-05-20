16b9bbb06ed2fdf50a2fbb3a0fb4dfe074355125
Olga <nadolga.nl@gmail.com>
2022-05-20 14:01:29 +0200

    addMarvelServiceClass

---
+++ src/services/MarvelService.js
@@ -0,0 +1,40 @@
+class MarvelService {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+
+    _apiKey = 'apikey=3318def779a4ba0fa52b596396c91967';
+    _basisOffset = 210;
+
+    getResource = async (url) => {
+        let res = await fetch(url);
+    
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+    
+        return await res.json();
+    }
+
+    getAllCharacters = async (offset=this._basisOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
+    }
+
+    getCharacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return {
+            name: char.name,
+            description: char.description,
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki:char.urls[1].url,
+            id: char.id,
+            comics: char.comics.items
+        }
+    }
+}
+
+export default MarvelService;
